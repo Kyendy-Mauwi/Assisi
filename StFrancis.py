82c50fa9d9cc022a70ed2d2628368b046804d4d0
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2023-10-03 16:01:04 +0300

    Update StFrancis.py

--- StFrancis.py
+++ StFrancis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 from PIL import Image
+import base64
 
 # Set the page title and icon
 st.set_page_config(page_title="St. Francis Youth AGM", page_icon="SFY.png")  # Replace "SFY.png" with the correct file path
@@ -21,15 +22,16 @@ with col2:
 # Define the admin password
 admin_password = "Sfy2023"  # Replace with your desired password
 
-# Create a text input for password
-password = st.text_input("Enter the admin password:", type="password")
+# Create a checkbox for admin access
+is_admin = st.checkbox("Log In as Admin")
 
-# Check if the password is correct
-if password == admin_password:
-    st.success("You are logged in as the admin.")
-    admin_logged_in = True
-else:
-    admin_logged_in = False
+if is_admin:
+    # If the user wants admin access, ask for the admin password
+    password_input = st.text_input("Enter the admin password:", type="password")
+
+    if password_input != admin_password:
+        st.warning("Please enter the correct password to access admin privileges.")
+        is_admin = False
 
 # Define the path to the CSV file
 csv_file_path = "Count.csv"
@@ -40,19 +42,16 @@ if os.path.exists(csv_file_path):
 else:
     df = pd.DataFrame(columns=["Name", "Amount"])
 
-# Display the CSV data outside of the admin sections
-st.header("CSV Data")
-st.dataframe(df)
-
-# If the admin is logged in, allow data management
-if admin_logged_in:
+# Check if the user is logged in as admin
+if is_admin:
+    # Admin has access to data management
     # User input section to add or update data
-    st.header("Add or Update Data")
+    st.header("Add Member")
 
     name = st.text_input("Name:")
     amount = st.number_input("Contribution Amount:", value=0)
 
-    if st.button("Add or Update"):
+    if st.button("Add"):
         if name and amount > 0:
             new_data = pd.DataFrame({"Name": [name], "Amount": [amount]})
             df = pd.concat([df, new_data], ignore_index=True)
@@ -62,18 +61,49 @@ if admin_logged_in:
             st.error("Please enter a valid name and a contribution amount greater than 0.")
 
     # User input section to delete data
-    st.header("Delete Data")
+    st.header("Delete Member")
 
     # Display the current data for deletion
     if not df.empty:
         selected_data = st.selectbox("Select data to delete", df["Name"])
-        if st.button("Delete Selected Data"):
+        if st.button("Delete Selected Member"):
             df = df[df["Name"] != selected_data]
             df.to_csv(csv_file_path, index=False)
             st.success(f"{selected_data} deleted successfully!")
+
+# Display the CSV data outside of the admin sections
+st.header("Member Contributions")
+st.dataframe(df)
 
 # Display the CSV data and total contribution amount
 st.header("Total Contribution Amount")
 if "Amount" in df.columns:
     total_contributions = df["Amount"].sum()
     st.write(f"The total contribution amount is: ${total_contributions:.2f}")
+
+# Allow all users to download the CSV file
+st.write('For copy click below⬇️')
+csv_file = df.to_csv(index=False).encode()
+b64 = base64.b64encode(csv_file).decode()
+st.markdown(f'<a href="data:file/csv;base64,{b64}" download="Count.csv">Download Members List</a>', unsafe_allow_html=True)
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+# Centered and sticky footer using CSS
+footer = """
+<style>
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+}
+</style>
+<div class="footer">
+    <p>Copyright SFY 2023</p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
