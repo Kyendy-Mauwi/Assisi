63310d74259c88fa60bbf6f5c514a4dad07fb137
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2023-10-03 13:33:47 +0300

    Update StFrancis.py

--- StFrancis.py
+++ StFrancis.py
@@ -12,23 +12,27 @@ col1, col2 = st.columns(2)
 # Logo
 with col1:
     logo = Image.open("SFY.png")
-    st.image(logo, width=90) 
+    st.image(logo, width=100)  # Adjust the width as needed
 
 # Page title
 with col2:
-    st.title("St. Francis Youth")
+    st.title("St. Francis Youth AGM")
+
+# Define the admin password
+admin_password = "Sfy2023"  # Replace with your desired password
+
+# Create a text input for password
+password = st.text_input("Enter the admin password:", type="password")
+
+# Check if the password is correct
+if password == admin_password:
+    st.success("You are logged in as the admin.")
+    admin_logged_in = True
+else:
+    admin_logged_in = False
 
 # Define the path to the CSV file
 csv_file_path = "Count.csv"
-
-# Function to read and display the CSV data
-def read_and_display_csv(data_frame):
-    st.write("Members:")
-    st.dataframe(data_frame)
-    if "Amount" in data_frame.columns:
-        total_contributions = data_frame["Amount"].sum()
-        st.header("Figures😁")
-        st.write(f"Tuko na: KES {total_contributions:.2f}")
 
 # Load the initial "Count.csv" file if it exists
 if os.path.exists(csv_file_path):
@@ -36,31 +40,40 @@ if os.path.exists(csv_file_path):
 else:
     df = pd.DataFrame(columns=["Name", "Amount"])
 
-# User input section to add or update data
-st.header("Join sherehe na thao")
+# Display the CSV data outside of the admin sections
+st.header("CSV Data")
+st.dataframe(df)
 
-name = st.text_input("Name:")
-amount = st.number_input("Contribution Amount:", value=1000)
+# If the admin is logged in, allow data management
+if admin_logged_in:
+    # User input section to add or update data
+    st.header("Add or Update Data")
 
-if st.button("Weka ndani🎉"):
-    if name and amount > 0:
-        new_data = pd.DataFrame({"Name": [name], "Amount": [amount]})
-        df = pd.concat([df, new_data], ignore_index=True)
-        df.to_csv(csv_file_path, index=False)
-        st.success("Data added or updated successfully!")
-    else:
-        st.error("Please enter a valid name and a contribution amount greater than 0.")
+    name = st.text_input("Name:")
+    amount = st.number_input("Contribution Amount:", value=0)
 
-# User input section to delete data
-st.header("Who's lefting")
+    if st.button("Add or Update"):
+        if name and amount > 0:
+            new_data = pd.DataFrame({"Name": [name], "Amount": [amount]})
+            df = pd.concat([df, new_data], ignore_index=True)
+            df.to_csv(csv_file_path, index=False)
+            st.success("Data added or updated successfully!")
+        else:
+            st.error("Please enter a valid name and a contribution amount greater than 0.")
 
-# Display the current data for deletion
-if not df.empty:
-    selected_data = st.selectbox("Tunachuja nani?", df["Name"])
-    if st.button("Baaeeh😭"):
-        df = df[df["Name"] != selected_data]
-        df.to_csv(csv_file_path, index=False)
-        st.success(f"{selected_data} deleted successfully!")
+    # User input section to delete data
+    st.header("Delete Data")
+
+    # Display the current data for deletion
+    if not df.empty:
+        selected_data = st.selectbox("Select data to delete", df["Name"])
+        if st.button("Delete Selected Data"):
+            df = df[df["Name"] != selected_data]
+            df.to_csv(csv_file_path, index=False)
+            st.success(f"{selected_data} deleted successfully!")
 
 # Display the CSV data and total contribution amount
-read_and_display_csv(df)
+st.header("Total Contribution Amount")
+if "Amount" in df.columns:
+    total_contributions = df["Amount"].sum()
+    st.write(f"The total contribution amount is: ${total_contributions:.2f}")
